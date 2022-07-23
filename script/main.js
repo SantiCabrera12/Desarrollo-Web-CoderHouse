51ccce14491bd9e5df92dbc4044cde96d289233d
Santi Cabrera <sanntiagocabrera2001@gmail.com>
2022-07-23 19:41:12 -0300

    cambio en el scss

--- script/main.js
+++ script/main.js
@@ -1,13 +1,13 @@
 
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.querySelector(".header__hamburger")
+const navMenu = document.querySelector(".header__menu")
 
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active')
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",() =>{
+document.querySelectorAll(".menu__link").forEach(n => n.addEventListener("click",() =>{
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
